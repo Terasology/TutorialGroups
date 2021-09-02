297caf9d2c29b52e06bb277b680250d05f91d32c
Tobias Nett <skaldarnar@googlemail.com>
2021-09-03 00:24:37 +0200

    style: fix remaining checkstyle warnings

--- src/main/java/org/terasology/behaviors/system/GroupBehaviorsEventSystem.java
+++ src/main/java/org/terasology/behaviors/system/GroupBehaviorsEventSystem.java
@@ -1,20 +1,8 @@
-/*
- * Copyright 2019 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.behaviors.system;
 
+import org.terasology.behaviors.components.FleeingComponent;
 import org.terasology.behaviors.components.GroupFleeOnHitComponent;
 import org.terasology.engine.core.Time;
 import org.terasology.engine.entitySystem.entity.EntityManager;
@@ -25,9 +13,8 @@ import org.terasology.engine.entitySystem.systems.RegisterMode;
 import org.terasology.engine.entitySystem.systems.RegisterSystem;
 import org.terasology.engine.logic.behavior.GroupTagComponent;
 import org.terasology.engine.logic.characters.CharacterMovementComponent;
-import org.terasology.module.health.events.OnDamagedEvent;
 import org.terasology.engine.registry.In;
-import org.terasology.behaviors.components.FleeingComponent;
+import org.terasology.module.health.events.OnDamagedEvent;
 
 /*
  * Listens for damage events and responds according to the group behavior desired
