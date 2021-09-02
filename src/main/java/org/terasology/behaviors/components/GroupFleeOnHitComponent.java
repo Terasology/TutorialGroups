d3b56c051b7571cf4af26d83f8af41c12413f104
Nail Khanipov <foxflameless@gmail.com>
2021-09-03 01:34:38 +0300

    chore: Update tutorials to lastest engine (#1)
    * chore(tutorial): migrate tutorial modules with engine package movement
    
    * chore(tutorial): migrate components to Gestaltv7
    
    * style: fix remaining checkstyle warnings
    
    * style: fix remaining checkstyle warnings
    
    Co-authored-by: Tobias Nett <skaldarnar@googlemail.com>

--- src/main/java/org/terasology/behaviors/components/GroupFleeOnHitComponent.java
+++ src/main/java/org/terasology/behaviors/components/GroupFleeOnHitComponent.java
@@ -1,21 +1,8 @@
-/*
- * Copyright 2019 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.behaviors.components;
 
-import org.terasology.entitySystem.Component;
+import org.terasology.gestalt.entitysystem.component.Component;
 
 /**
  * If this components is attached to an NPC entity it will exhibit the flee-on-hit behavior
@@ -24,9 +11,15 @@ import org.terasology.entitySystem.Component;
  * When it reaches a safe distance the instigator is set to null. This component uses
  * @see FleeOnHitComponent as a reference/basis.
  */
-public class GroupFleeOnHitComponent implements Component {
+public class GroupFleeOnHitComponent implements Component<GroupFleeOnHitComponent> {
     /* Minimum distance from instigator after which the NPC will stop 'flee'ing */
     public float minDistance = 10f;
     /* Speed factor by which flee speed increases */
     public float speedMultiplier = 1.2f;
+
+    @Override
+    public void copyFrom(GroupFleeOnHitComponent other) {
+        this.minDistance = other.minDistance;
+        this.speedMultiplier = other.speedMultiplier;
+    }
 }
